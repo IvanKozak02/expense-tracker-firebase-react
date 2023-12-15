adc77257da36e95c85eaa44797211cae93335bc7
Ivan Kozak <ivasykkozak611@gmail.com>
2023-12-15 22:09:52 +0200

    fixed bug

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,4 +1,4 @@
 export const useGetUserInfo =() =>{
-    const {userId,name, profileImg, isAuthenticated} = JSON.parse(localStorage.getItem('auth'));
+    const {userId,name, profileImg, isAuthenticated} = JSON.parse(localStorage.getItem('auth')) || {};
     return {userId,name, profileImg, isAuthenticated};
 }
